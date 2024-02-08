00aa4a46ed9a3dfec76e8d0acffb42f01a72071b
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-02-08 16:48:05 +0530

    dummy header section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-// import './App.css'
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,10 @@
+import LogoImg from "../../assets/logo.svg";
+
+const Logo = () => {
+  return (
+    <a href="">
+      <img className="h-9" src={LogoImg} alt="Weather App" />
+    </a>
+  );
+};
+export default Logo;
